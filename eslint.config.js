adc8c20726e913449333a2f713fee99040405c03
foolish-pine <matsuda.takuya41@gmail.com>
2024-08-12 14:35:48 +0900

    chore: use @shopify/eslint-plugin

---
+++ eslint.config.js
@@ -0,0 +1,18 @@
+import shopifyEslintPlugin from '@shopify/eslint-plugin';
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default [
+  {
+    ignores: ['assets/**'],
+  },
+  ...shopifyEslintPlugin.configs.typescript,
+  ...shopifyEslintPlugin.configs['typescript-type-checking'],
+  {
+    languageOptions: {
+      parserOptions: {
+        project: 'tsconfig.json',
+      },
+    },
+  },
+  ...shopifyEslintPlugin.configs.prettier,
+];
--- src/scripts/test.ts
+++ src/scripts/test.ts
@@ -1,3 +1,3 @@
 const hello = 'Hello';
 
-console.log(hello);
+// console.log(hello);
