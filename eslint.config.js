0c00dcc8c2fd5f3702130b0c995e7e985a4836b3
foolish-pine <matsuda.takuya41@gmail.com>
2024-08-14 14:04:43 +0900

    chore: use simple-import-sort

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,5 @@
 import shopifyEslintPlugin from "@shopify/eslint-plugin";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
 
 const config = [
   {
@@ -11,6 +12,13 @@ const config = [
       parserOptions: {
         project: "tsconfig.json",
       },
+    },
+    plugins: {
+      "simple-import-sort": simpleImportSort,
+    },
+    rules: {
+      "simple-import-sort/imports": "error",
+      "simple-import-sort/exports": "error",
     },
   },
   ...shopifyEslintPlugin.configs.prettier,
